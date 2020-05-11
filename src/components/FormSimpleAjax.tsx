53b7ccfa9ac1eb86c3585a89e6b772a8bad94354
malteA <malte@mallau.net>
2020-05-11 23:28:51 +0200

    #19 removed gender input from form

--- src/components/FormSimpleAjax.tsx
+++ src/components/FormSimpleAjax.tsx
@@ -1,32 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, FC, useState } from 'react'
 import Helmet from 'react-helmet'
 import { stringify } from 'qs'
 import { serialize } from 'dom-form-serializer'
 
 import './Form.css'
 
-class Form extends React.Component {
-  static defaultProps = {
-    name: 'Simple Form Ajax',
-    subject: '', // optional subject of the notification email
-    action: '',
-    successMessage: 'Thanks for your enquiry, we will get back to you soon',
-    errorMessage:
-      'There is a problem, your message has not been sent, please try contacting us via email'
-  }
+interface FormProps {
+  name: string;
+  subject: string;
+  action: string;
+  successMessage: string;
+  errorMessage: string;
+}
 
-  state = {
-    alert: '',
-    disabled: false
-  }
+const Form: FC<FormProps> = ({ name = "", subject = "", action = "", successMessage = "Thanks for your enquiry, we will get back to you soon", errorMessage = "There is a problem, your message has not been sent, please try contacting us via email" }) => {
+  const [alert, setAlert] = useState('');
+  const [disabled, setDisabled] = useState(false);
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
-    if (this.state.disabled) return
+    if (disabled) return
 
     const form = e.target
     const data = serialize(form)
-    this.setState({ disabled: true })
+    setDisabled(true);
     fetch(form.action + '?' + stringify(data), {
       method: 'POST'
     })
@@ -39,141 +36,112 @@ class Form extends React.Component {
       })
       .then(() => {
         form.reset()
-        this.setState({
-          alert: this.props.successMessage,
-          disabled: false
-        })
+        setDisabled(false);
+        setAlert(successMessage);
       })
       .catch(err => {
         console.error(err)
-        this.setState({
-          disabled: false,
-          alert: this.props.errorMessage
-        })
+        setDisabled(false);
+        setAlert(errorMessage);
       })
   }
 
-  render() {
-    const { name, subject, action } = this.props
-
-    return (
-      <Fragment>
-        <Helmet>
-          <script src="https://www.google.com/recaptcha/api.js" />
-        </Helmet>
-        <form
-          className="Form"
-          name={name}
-          action={action}
-          onSubmit={this.handleSubmit}
-          data-netlify=""
-          netlify-recaptcha=""
-        >
-          {this.state.alert && (
-            <div className="Form--Alert">{this.state.alert}</div>
-          )}
-          <fieldset>
-            <label className="Form--Label Form--Radio">
-              <input
-                className="Form--RadioInput"
-                type="radio"
-                name="gender"
-                value="male"
-                defaultChecked
-              />
-              <span>Herr</span>
-            </label>
-            <label className="Form--Label Form--Radio">
-              <input
-                className="Form--RadioInput"
-                type="radio"
-                name="gender"
-                value="female"
-              />
-              <span>Frau</span>
-            </label>
-          </fieldset>
-          <div className="Form--Group">
-            <label className="Form--Label">
-              <input
-                className="Form--Input Form--InputText"
-                type="text"
-                placeholder="Vorname"
-                name="firstname"
-                required
-              />
-              <span>Vorname</span>
-            </label>
-            <label className="Form--Label">
-              <input
-                className="Form--Input Form--InputText"
-                type="text"
-                placeholder="Nachname"
-                name="lastname"
-                required
-              />
-              <span>Nachname</span>
-            </label>
-          </div>
+  return (
+    <Fragment>
+      <Helmet>
+        <script src="https://www.google.com/recaptcha/api.js" />
+      </Helmet>
+      <form
+        className="Form"
+        name={name}
+        action={action}
+        onSubmit={handleSubmit}
+        data-netlify=""
+        netlify-recaptcha=""
+      >
+        {alert && (
+          <div className="Form--Alert">{alert}</div>
+        )}
+        <div className="Form--Group">
           <label className="Form--Label">
             <input
               className="Form--Input Form--InputText"
-              type="email"
-              placeholder="Email"
-              name="emailAddress"
+              type="text"
+              placeholder="Vorname"
+              name="firstname"
               required
             />
-            <span>Email</span>
-          </label>
-          <label className="Form--Label has-arrow">
-            <select
-              className="Form--Input Form--Select"
-              name="type"
-              defaultValue="Type of Enquiry"
-              required
-            >
-              <option disabled hidden>
-                Kontaktgrund
-              </option>
-              <option>Need to know more</option>
-              <option>Found a bug</option>
-              <option>Want to say hello</option>
-            </select>
+            <span>Vorname</span>
           </label>
           <label className="Form--Label">
-            <textarea
-              className="Form--Input Form--Textarea Form--InputText"
-              placeholder="Message"
-              name="message"
-              rows="10"
+            <input
+              className="Form--Input Form--InputText"
+              type="text"
+              placeholder="Nachname"
+              name="lastname"
               required
             />
-            <span>Nachricht</span>
+            <span>Nachname</span>
           </label>
-          <label className="Form--Label Form-Checkbox">
-            <input
-              className="Form--Input Form--Textarea Form--CheckboxInput"
-              name="newsletter"
-              type="checkbox"
-            />
-            <span>Newsletter</span>
-          </label>
-          <div
-            className="g-recaptcha"
-            data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
-          />
-          {!!subject && <input type="hidden" name="subject" value={subject} />}
-          <input type="hidden" name="form-name" value={name} />
+        </div>
+        <label className="Form--Label">
           <input
-            className="Button Form--SubmitButton"
-            type="submit"
-            value="Enquire"
-            disabled={this.state.disabled}
+            className="Form--Input Form--InputText"
+            type="email"
+            placeholder="Email"
+            name="emailAddress"
+            required
           />
-        </form>
-      </Fragment>
-    )
-  }
+          <span>Email</span>
+        </label>
+        <label className="Form--Label has-arrow">
+          <select
+            className="Form--Input Form--Select"
+            name="type"
+            defaultValue="Type of Enquiry"
+            required
+          >
+            <option disabled hidden>
+              Kontaktgrund
+              </option>
+            <option>Need to know more</option>
+            <option>Found a bug</option>
+            <option>Want to say hello</option>
+          </select>
+        </label>
+        <label className="Form--Label">
+          <textarea
+            className="Form--Input Form--Textarea Form--InputText"
+            placeholder="Message"
+            name="message"
+            rows={10}
+            required
+          />
+          <span>Nachricht</span>
+        </label>
+        <label className="Form--Label Form-Checkbox">
+          <input
+            className="Form--Input Form--Textarea Form--CheckboxInput"
+            name="newsletter"
+            type="checkbox"
+          />
+          <span>Newsletter</span>
+        </label>
+        <div
+          className="g-recaptcha"
+          data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
+        />
+        {!!subject && <input type="hidden" name="subject" value={subject} />}
+        <input type="hidden" name="form-name" value={name} />
+        <input
+          className="Button Form--SubmitButton"
+          type="submit"
+          value="Enquire"
+          disabled={disabled}
+        />
+      </form>
+    </Fragment>
+  )
 }
 
 export default Form
