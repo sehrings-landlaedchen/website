2c93a46faaa69ab26833112151a665c439e84c10
malteA <malte@mallau.net>
2020-05-16 01:13:51 +0200

    leafleat-map typescript

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Map, TileLayer, Popup, Marker } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet';
 
-const LeafletMap = props => {
-  const position = [props.location.lat, props.location.lng];
+interface LeafletMapProps {
+  location: {
+    lat: number;
+    lng: number;
+  }
+  zoom: number
+}
+
+const LeafletMap: FC<LeafletMapProps> = props => {
+  const position: LatLngExpression = [props.location.lat, props.location.lng];
   return (
-      <Map center={position} zoom={props.zoom} style={{ height: '50vh', width: '100%', zIndex: 10 }} scrollWheelZoom={false}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>Sehrings Landlädchen</Popup>
-        </Marker>
-      </Map>
+    <Map
+      center={position}
+      zoom={props.zoom}
+      style={{ height: '50vh', width: '100%', zIndex: 10 }}
+      scrollWheelZoom={false}
+      >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={position}>
+        <Popup>Sehrings Landlädchen</Popup>
+      </Marker>
+    </Map>
   )
 }
 LeafletMap.defaultProps = {
