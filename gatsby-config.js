3cbcf2c7d9f592833391e6d9f22c3338b9e2fba9
malteA <malte@mallau.net>
2021-06-24 09:25:45 +0200

    fix: gatsyby-plugin-sitemap excludes

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
           {
             resolve: `gatsby-plugin-sitemap`,
             options: {
-              exclude: [
+              excludes: [
                 `/products`,
                 `/products/*`,
                 `/product-categories/*`,
