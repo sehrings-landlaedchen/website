5c9c8160fea792ae0086db0d12a6699d43885099
Malte Arzt <malte@mallau.net>
2022-10-16 22:11:01 +0200

    chore: removed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,12 +89,6 @@ module.exports = {
         icon: `${__dirname}/static/img/logo.svg` // This path is relative to the root of the site.
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-typography`,
-    //   options: {
-    //     pathToConfigModule: `src/utils/typography`,
-    //   },
-    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
